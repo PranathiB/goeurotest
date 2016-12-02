289398c934fd6a7feaa06e11c28584337eb6ad89
Pranathi Birudugadda <pranathb@thoughtworks.com>
2016-12-02 15:06:24 +0530

    @pb: switching to chromedriver

--- src/test/SearchWidgetTest.java
+++ src/test/SearchWidgetTest.java
@@ -3,11 +3,12 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import pages.GoEuroHomePage;
-import pages.SearchResultsPage;
-import pages.SearchWidget;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.pages.SearchResultsPage;
+import java.pages.SearchWidget;
+import java.pages.GoEuroHomePage;
 
 /**
  * Created by pranathb on 11/29/16.
@@ -19,8 +20,10 @@ public class SearchWidgetTest {
 
     @BeforeClass
     public static void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
+        System.setProperty("webdriver.chrome.driver", "src/resources/driver/chromedriver");
+        driver = new ChromeDriver();
+        Dimension dimension = new Dimension(1440, 900);
+        driver.manage().window().setSize(dimension);
     }
 
     @Test
@@ -29,19 +32,16 @@ public class SearchWidgetTest {
         goEuroHomePage.go();
         searchWidget = new SearchWidget(driver);
         searchWidget.enterOriginCity("Berlín");
-
         searchWidget.enterDestinationCity("Praga");
-
         searchWidget.uncheckAirbnbOption();
         searchWidget.clickOnSearchButton();
-
         SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
         Assert.assertTrue(Ordering.natural().isOrdered(searchResultsPage.getAllPrices()));
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
-        driver.close();
+        driver.quit();
     }
 
 }
